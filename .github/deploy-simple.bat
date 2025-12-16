a8c3d307b9b9c118c096348244328352bc0e2570
partner0010 <partner0010@gmail.com>
2025-12-16 21:52:30 +0900

    fix: Button 컴포넌트 motion.button을 motion.div로 감싸서 타입 충돌 해결

--- .github/deploy-simple.bat
+++ .github/deploy-simple.bat
@@ -2,6 +2,6 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 git add .
-git commit -m "fix: Button 컴포넌트 motion.button 타입 충돌 해결"
+git commit -m "fix: Button 컴포넌트 motion.button을 motion.div로 감싸서 타입 충돌 해결"
 git push origin main
 pause
